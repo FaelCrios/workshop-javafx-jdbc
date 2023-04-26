3e07f52c0b4fa465123f6db9538a2804cbd4f5f6
Rafael Colin <faelnek@gmail.com>
2023-04-26 14:57:46 -0300

    About view and controllers, Alerts, Constraints

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,12 +9,16 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
@@ -27,7 +31,9 @@ public class Main extends Application {
 		}
 	}
 
-
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 
 	public static void main(String[] args) {
 		launch(args);
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -34,7 +43,7 @@ public class MainViewController implements Initializable{
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
@@ -43,4 +52,36 @@ public class MainViewController implements Initializable{
 		
 	}
 
+	//Como o nosso processo é multiThread sendo por causa da javaFx, o synchronized entra para proibir que nosso processo
+	//seja interrompido
+	private synchronized void loadView(String absoluteName) {
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		VBox newVBox = loader.load();
+		
+		//Aqui criamos uma referencia para nossa cena principal
+		Scene mainScene = Main.getMainScene();
+		
+		//Aqui nós pegamos o conteudo que está dentro dela
+		//(ScrollPane)mainScene.getRoot()) nós pegamos o primeiro filho da scene e cascamos ele para ScrollPane que é oque ele é
+		//Após isso com (VBox)((ScrollPane)mainScene.getRoot()).getContent(); nós pegamos o conteúdo presente dentro dele e castamos
+		//Para VBox sendo essa a scene que nós vamos utilizar para manipular.
+		
+		VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+		
+		//Selecionamos o item que queremos salvar, no caso esse menu
+		Node mainMenu = mainVBox.getChildren().get(0);
+		//excluimos o conteudo da antiga página 
+		mainVBox.getChildren().clear();
+		
+		//E por fim adicionamos o conteudo Menu, e o conteudo da página que desejamos adicionar
+		mainVBox.getChildren().add(mainMenu);
+		mainVBox.getChildren().addAll(newVBox.getChildren());
+		
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		}
+	
 }
