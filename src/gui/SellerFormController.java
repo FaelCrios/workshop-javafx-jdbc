a956d1116400a823f9c10178901d2f9c09aac534
Rafael Colin <faelnek@gmail.com>
2023-05-04 11:39:23 -0300

    Error handling and Saving Seller on DB

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -150,6 +152,27 @@ public class SellerFormController implements Initializable {
 		}
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addErrors("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		// Transformando uam data DatePicker que tem zone para uma qualquer
+		if (dpBirthDate.getValue() == null) {
+			exception.addErrors("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addErrors("baseSalary", "Field can't be empty");
+		} else {
+
+			obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		}
+		
+		obj.setDepartment(comboBoxDepartment.getValue());		
+
 		// Se a nossa lista de erros tiver algum erro.. ele barra
 		if (exception.getErrors().size() > 0) {
 			throw exception;
@@ -198,7 +221,7 @@ public class SellerFormController implements Initializable {
 		if (entity.getBirthDate() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
-		if(entity.getDepartment() == null) {
+		if (entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
 		}
 		comboBoxDepartment.setValue(entity.getDepartment());
@@ -207,10 +230,19 @@ public class SellerFormController implements Initializable {
 
 	private void setErrorMessages(Map<String, String> error) {
 		Set<String> fields = error.keySet();
-		if (fields.contains("name")) {
-			labelErrorName.setText(error.get("name"));
-		}
-		;
+		
+		//Operador ternario: Ou seja
+		//Se tiver a minha condicao for verdadeira eu coloco      V
+		//Caso ela for falsa eu coloco                                       V
+		labelErrorName.setText(fields.contains("name") ? error.get("name") : "");
+		
+		labelErrorEmail.setText(fields.contains("email") ? error.get("email") : "");
+
+		labelErrorBirthDate.setText(fields.contains("birthDate") ? error.get("birthDate") : "");
+		
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? error.get("baseSalary") : "");
+		
+
 	}
 
 	public void loadAssociatedObjects() {
